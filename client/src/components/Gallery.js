28f53d644bb1836072dc8388a13df5a7e47f1e3c
Keari Eggers <kungfu.keari@gmail.com>
2017-08-23 14:41:50 -0500

    add picture element for responsive images

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import { gql, graphql } from "react-apollo";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Canvas from "./Canvas";
 import Loader from "./Loader";
-import { pixelsToObject } from "../utils/pixelUtils";
 
 const Drawings = styled.div`
 display: flex;
@@ -15,23 +13,19 @@ align-items: flex-start;
 
 const DrawingLink = styled(Link)`
 display: block;
-border: 2px solid #ccc;
 margin: 10px 0;
 text-decoration: none;
 `;
 
-const getEmbedWidth = () => {
-  const width = window.innerWidth;
-  const baseFontSize = 16;
-  const borderWidth = 2;
-  return width > 800 ? 800 : width - (baseFontSize + borderWidth) * 2;
-};
+const Image = styled.img`
+  max-width: 100%;
+  border: 2px solid #ccc;
+`;
 
-const getPixels = drawing => {
-  const pixels = drawing.sections
-    .map(d => d.pixels)
-    .reduce((a, b) => a.concat(b), []);
-  return pixelsToObject(pixels, pixel => `${pixel.x},${pixel.y}`);
+const getUrl = (url, width) => {
+  return url.replace(/(upload\/)(\w+)/, (match, p1, p2) => {
+    return `${p1}w_${width}/f_auto,q_auto/${p2}`;
+  });
 };
 
 class Gallery extends Component {
@@ -62,16 +56,22 @@ class Gallery extends Component {
     return (
       <Drawings>
         {drawings.map(drawing => {
-          const url = drawing.imageUrl.replace(
-            /(upload\/)(\w+)/,
-            (match, p1, p2) => {
-              return `${p1}w_800/f_auto,q_auto/${p2}`;
-            }
-          );
-          console.log("url", url);
           return (
             <DrawingLink to={`/drawing/${drawing.id}`} key={drawing.id}>
-              <img src={url} alt={drawing.name} />
+              <picture>
+                <source
+                  srcSet={getUrl(drawing.imageUrl, 400)}
+                  media="(max-width: 400px)"
+                />
+                <source
+                  srcSet={getUrl(drawing.imageUrl, 600)}
+                  media="(min-width: 401px) and (max-width: 600px)"
+                />
+                <Image
+                  srcSet={getUrl(drawing.imageUrl, 800)}
+                  alt={drawing.name}
+                />
+              </picture>
             </DrawingLink>
           );
         })}
